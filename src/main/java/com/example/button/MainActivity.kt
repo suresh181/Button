516c542f5297b90f1639ffc92dcce98563c98016
B Suresh <80473048+suresh181@users.noreply.github.com>
2021-10-25 21:37:07 +0530

    Add files via upload

---
+++ src/main/java/com/example/button/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.button
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+          btnApply.setOnClickListener {
+              val firstName = etFirstName.text.toString()
+              val lastName = etLastName.text.toString()
+              val birthDate = etBirthDate.text.toString()
+              val country = etCountry.text.toString()
+              Log.d("MainActivity","$firstName $lastName born on $birthDate from $country just Applied")
+          }
+    }
+}
